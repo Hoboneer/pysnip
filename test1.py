70e74b972d39c5150d2cf3968e94ca5e9ca31178
Hoboneer <gabriel.lisaca@gmail.com>
2019-05-04 11:39:18 +1200

    Clean up test filesystem building
    This is a much cleaner way of handling paths and building up the
    filesystem from the input program.

--- test1.py
+++ test1.py
@@ -1,9 +1,14 @@
 from itertools import chain
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Union
+from pathlib import PurePosixPath
 
 import parso
-from parso.tree import Scope
+from parso.python.tree import Scope
 import fs
+
+
+# Does mypy support recursive types yet?
+Filesystem = Dict[PurePosixPath, Union['Filesystem', Scope]]
 
 
 # NOTE: Filename for the code text is simply "<path>/<name>.<type>".
@@ -11,44 +16,78 @@ import fs
 # NOTE: Filename for the directory containing children is simply
 # "<path>/<name>.<type>.c" ("c" for children).
 
-# TODO: What to do about duplicates?
-#       - Perhaps make a new directory with similar naming scheme? This forces
-#         users to choose which code text they want.
-#       - Naming scheme? "<path>/<name>.<type>.d" ("d" for duplicates).
-#         Perhaps filename should just be a (maybe, randomly chosen) number in
-#         order to differentiate.
+# NOTE: Filename for the directory containing definitions in the same scope is
+# simply "<path>/<name>.type.c" ("d" for duplicate). The child definitions will
+# be uniquely named (perhaps it should be a hash of their contents?), e.g., "0.classdef".
 
-def recurse_filter(tree: Scope, path: str) -> List[Tuple[str, Scope]] :
-    # Tree is a parso tree.
-    # Path is a POSIX path.
+def recurse_filter(tree: Scope, path: PurePosixPath) -> List[Tuple[PurePosixPath, Scope]]:
+    # Recursively construct a list mapping paths (in the code structure) to `Scope`s to `Scope`s.
     scopes = list(chain(tree.iter_funcdefs(), tree.iter_classdefs()))
     # Base case.
     if len(scopes) == 0:
         return []
 
-    path_to_scopes = list(map(lambda scope: (path, scope), scopes))
+    path_to_scopes = list(
+        map(lambda scope: (path / f"{scope.name.value}.{scope.type}", scope), scopes)
+    )
     # Recursively get children scopes.
-    for path, scope in path_to_scopes.copy():
+    for scope_path, scope in path_to_scopes.copy():
         extension = scope.type
-        path_to_scopes += recurse_filter(scope, f"{path}{scope.name.value}.{extension}.d/")
+        path_to_scopes += recurse_filter(
+            scope, path / f"{scope.name.value}.{extension}.c"
+        )  # "c" for children.
 
     return path_to_scopes
 
-# with fs.open_fs('.') as cwd:
-#     program = parso.parse(cwd.gettext('test.py'))
-#     funcdefs = program.iter_funcdefs()
-#     classdefs = program.iter_classdefs()
-#     for scopedef in chain(funcdefs, classdefs):
-#         extension = scopedef.type
-#         name = scopedef.name.value
-#         print(f"{name}.{extension}")
-#         print(scopedef.get_code(include_prefix=False))
+def paths_to_fs(path_to_scopes: List[Tuple[PurePosixPath, Scope]]) -> Filesystem:
+    # Converts result of `recurse_filter` to filesystem-like dict.
 
-with fs.open_fs('.') as cwd:
-    program = parso.parse(cwd.gettext('test.py'))
+    # Group by paths.
+    grouped_scopes: Dict[PurePosixPath, List[Scope]] = {}
+    for path, scope in sorted(path_to_scopes.copy(), key=lambda item: item[0]):
+        grouped_scopes.setdefault(path, []).append(scope)
+
+    # De-duplicate paths: Place duplicate definitions made in the same scope
+    # into a subdirectory (suffix: ".d" for duplicate).
+    fs_dirs: List[Tuple[PurePosixPath, Scope]] = []
+    for path, scopes in grouped_scopes.items():
+        # Scopes of length 1 can be safely moved out of the list (they are
+        # unique).
+        if len(scopes) == 1:
+            fs_dirs.append((path, scopes[0]))
+            continue
+
+        # Collapse multiple scopes into uniquely named files in their '.d' directory.
+        for i, scope in enumerate(scopes):
+            fs_dirs.append((path.with_suffix('.d') / f"{i}.{scope.type}", scope))
+
+    filesystem = {}
+    # Make fs directory hierarchy.
+    for path, scope in fs_dirs:
+        current_level = filesystem
+        for i, part in enumerate(path.parts):
+            # No subdirs need to be made for root dir.
+            # TODO: What to do about "weird" paths? (e.g., '/' parts in the middle).
+            if i == 0 and part == '/':
+                continue
+
+            # Make "file" in specified parent part if this is last part it is not a directory.
+            if part == path.name and not part.endswith(('.d', '.c')):
+                current_level[part] = scope
+                break
+
+            # Make directory if needed.
+            if part not in current_level:
+                current_level[part] = {}
+
+            # Go into subdirectory.
+            current_level = current_level[part]
+
+    return filesystem
+
+
+with fs.open_fs(".") as cwd:
+    program = parso.parse(cwd.gettext("test.py"))
     tree = program
-    paths_to_scopes = recurse_filter(tree, "/")
-    for path, scope in paths_to_scopes:
-        print(path)
-        print(scope.get_code(include_prefix=False))
-    print(paths_to_scopes)
+    paths_to_scopes = recurse_filter(tree, PurePosixPath("/"))
+    filesystem = paths_to_fs(paths_to_scopes)
