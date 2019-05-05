717889ad4fa6953c5845b4512203625e5025a4d9
Hoboneer <gabriel.lisaca@gmail.com>
2019-05-05 20:04:12 +1200

    Have two internal representations of filesystem
    This makes the case of accessing (existing) files fast and falls back to
    walking the filesystem if accessing directories or nonexistent files.
    
    Also refactors `_get_file_at_path` to raise `ResourceNotFound` instead
    of return `None`.

--- pyfs.py
+++ pyfs.py
@@ -40,11 +40,8 @@ from parso.python.tree import Scope
 # be uniquely named (perhaps it should be a hash of their contents?), e.g.,
 # "0.classdef".
 
-# TODO: Make internal representation of filesystem flat. This makes looking up
-# files quicker (O(1) vs O(n)).
-
-# Does mypy support recursive types yet?
-Filesystem = Dict[PurePosixPath, Union["Filesystem", Scope]]
+# mypy does not support recursive types yet.
+Filesystem = Dict[PurePosixPath, Union["Filesystem", Scope]]  # type: ignore
 RawInfo = Mapping[str, Mapping[str, object]]
 
 
@@ -74,18 +71,17 @@ def map_code_tree(
     return path_to_scopes
 
 
-def paths_to_fs(
-    paths_to_scopes: List[Tuple[PurePosixPath, Scope]]
-) -> Filesystem:
-    # Converts result of `map_code_tree` to filesystem-like dict.
-
+def dedup_paths(
+    paths: List[Tuple[PurePosixPath, Scope]]
+) -> List[Tuple[PurePosixPath, Scope]]:
     # Group by paths.
     grouped_scopes: Dict[PurePosixPath, List[Scope]] = {}
-    for path, scope in paths_to_scopes:
+    for path, scope in paths:
         grouped_scopes.setdefault(path, []).append(scope)
 
     # De-duplicate paths: Place duplicate definitions made in the same scope
-    # into a subdirectory (suffix: ".d" for duplicate).
+    # into a subdirectory (suffix: ".d" for duplicate). Note, this is still a
+    # flat "filesystem".
     fs_dirs: List[Tuple[PurePosixPath, Scope]] = []
     for path, scopes in grouped_scopes.items():
         # Scopes of length 1 can be safely moved out of the list (they are
@@ -100,30 +96,33 @@ def paths_to_fs(
             fs_dirs.append(
                 (path.with_suffix(".d") / f"{i}.{scope.type}", scope)
             )
+    return fs_dirs
 
+
+def make_fs(paths: List[Tuple[PurePosixPath, Scope]]) -> Filesystem:
+    # Converts list of path-scope pairs to filesystem-like dict.
     filesystem: Filesystem = {}
     # Make fs directory hierarchy.
-    for path, scope in fs_dirs:
+    for path, scope in paths:
         current_level = filesystem
-        for i, part in enumerate(path.parts):
+        for part in path.parts:
             # No subdirs need to be made for root dir.
-            # TODO: What to do about "weird" paths? (e.g., '/' parts in the
-            # middle).
-            if i == 0 and part == "/":
+            if part == "/":
                 continue
 
-            # Make "file" in specified parent part if this is last part it is
-            # not a directory.
+            posix_part = PurePosixPath(part)
+            # Make "file" in specified parent part if this is last part and it
+            # is not a directory.
             if part == path.name and not part.endswith((".d", ".c")):
-                current_level[PurePosixPath(part)] = scope
+                current_level[posix_part] = scope
                 break
 
             # Make directory if needed.
-            if PurePosixPath(part) not in current_level:
-                current_level[PurePosixPath(part)] = {}
+            if posix_part not in current_level:
+                current_level[posix_part] = {}
 
             # Go into subdirectory.
-            current_level = current_level[PurePosixPath(part)]
+            current_level = current_level[posix_part]
 
     return filesystem
 
@@ -143,33 +142,40 @@ class PyCodeFS(FS):
     def __init__(self, code_text: str, language_version: str = "3.6"):
         super().__init__()
         self._program = parso.parse(code_text, version=language_version)
-        self._filesystem: Filesystem = self._build_filesystem()
+        paths_to_scopes = self._build_filesystem()
+        # For quick (direct) access of files (O(1) vs O(n)).
+        self._files: Dict[PurePosixPath, Scope] = dict(paths_to_scopes)
+        # For walking the filesystem.
+        self._filesystem: Filesystem = make_fs(paths_to_scopes)
 
-    def _build_filesystem(self) -> Filesystem:
+    def _build_filesystem(self) -> List[Tuple[PurePosixPath, Scope]]:
         code_tree = self._program.get_root_node()
         # Start at root directory (top-level/module scope)
         paths_to_scopes = map_code_tree(code_tree, PurePosixPath("/"))
-        return paths_to_fs(paths_to_scopes)
+        return dedup_paths(paths_to_scopes)
 
-    def _get_file_at_path(
-        self, path: str
-    ) -> Optional[Tuple[PurePosixPath, Filesystem]]:
-        # Walk through `path` in filesystem.
+    def _get_file_at_path(self, path: str) -> Tuple[PurePosixPath, Filesystem]:
         path_obj = PurePosixPath(path)
-        current_level = self._filesystem
         try:
-            for i, part in enumerate(path_obj.parts):
-                if part == "/":
-                    continue
-                current_level = current_level[PurePosixPath(part)]
+            resource = self._files[path_obj]
         except KeyError:
-            return None
+            # Attempting to access a directory.
+            # Walk through `path` in filesystem.
+            current_level = self._filesystem
+            try:
+                for part in path_obj.parts:
+                    # Root directory does not exist in internal representation
+                    # of filesystem.
+                    if part == "/":
+                        continue
+                    current_level = current_level[PurePosixPath(part)]
+            except KeyError:
+                raise ResourceNotFound(path)
 
-        # At this point, a valid "file" should be stored in `current_level`.
-        resource = current_level
+            # At this point, a valid "file" should be stored in `current_level`.
+            resource = current_level
 
-        filesystem = (path_obj, resource)
-        return filesystem
+        return (path_obj, resource)
 
     def getinfo(
         self, path: str, namespaces: Optional[Collection[str]] = None
@@ -186,16 +192,14 @@ class PyCodeFS(FS):
                     "TODO: What to do about unsupported namespaces?"
                 )
 
-        file_query = self._get_file_at_path(path)
-        if file_query is None:
-            raise ResourceNotFound(path)
-        path_obj, resource = file_query
+        path_obj, resource = self._get_file_at_path(path)
 
         raw_info = {
             "basic": {
                 # Special-case root-dir.
                 "name": "/" if path == "/" else path_obj.name,
                 "is_dir": path == "/"
+                # Resource is a directory if it suffixes include '.d' or '.c'.
                 or len(set((".d", ".c")) & set(path_obj.suffixes)) > 0,
             }
         }
@@ -205,9 +209,7 @@ class PyCodeFS(FS):
         info = self.getinfo(path)
         if not info.is_dir:
             raise DirectoryExpected(path)
-        file_query = self._get_file_at_path(path)
-        assert file_query is not None
-        path_obj, resource = file_query
+        path_obj, resource = self._get_file_at_path(path)
         local_files = [str(filename) for filename in resource.keys()]
         return local_files
 
@@ -229,9 +231,7 @@ class PyCodeFS(FS):
             raise Unsupported(msg="cannot modify pyfs")
         if "x" in mode:
             raise Unsupported(msg="cannot get exclusive control of resource")
-        file_query = self._get_file_at_path(path)
-        assert file_query is not None
-        _, resource = file_query
+        _, resource = self._get_file_at_path(path)
         return BytesIO(
             bytes(
                 cast(Scope, resource).get_code(include_prefix=False),
