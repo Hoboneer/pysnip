6f5682d2562bec4a96e1ea9d4ee83df041632774
Hoboneer <gabriel.lisaca@gmail.com>
2019-05-05 18:27:52 +1200

    Add type annotations and reformat code

--- pyfs.py
+++ pyfs.py
@@ -1,20 +1,31 @@
-from itertools import chain
-from typing import List, Tuple, Dict, Union, Optional, cast
-from pathlib import PurePosixPath
 from io import BytesIO
+from itertools import chain
+from pathlib import PurePosixPath
+from typing import (
+    Any,
+    BinaryIO,
+    Collection,
+    Dict,
+    List,
+    Mapping,
+    Optional,
+    Tuple,
+    Union,
+    cast,
+)
 
-from parso.python.tree import Scope
-import fs
+import parso
 from fs.base import FS
-from fs.info import Info
 from fs.errors import (
     DirectoryExpected,
+    FileExpected,
     ResourceNotFound,
     Unsupported,
-    FileExpected,
 )
-from fs import open_fs
-import parso
+from fs.info import Info
+from fs.permissions import Permissions
+from fs.subfs import SubFS
+from parso.python.tree import Scope
 
 # TODO: Should imports be implemented? Should they be directories in this
 # filesystem?
@@ -26,16 +37,22 @@ import parso
 
 # NOTE: Filename for the directory containing definitions in the same scope is
 # simply "<path>/<name>.type.c" ("d" for duplicate). The child definitions will
-# be uniquely named (perhaps it should be a hash of their contents?), e.g., "0.classdef".
+# be uniquely named (perhaps it should be a hash of their contents?), e.g.,
+# "0.classdef".
+
+# TODO: Make internal representation of filesystem flat. This makes looking up
+# files quicker (O(1) vs O(n)).
 
 # Does mypy support recursive types yet?
 Filesystem = Dict[PurePosixPath, Union["Filesystem", Scope]]
+RawInfo = Mapping[str, Mapping[str, object]]
 
 
 def map_code_tree(
     tree: Scope, path: PurePosixPath
 ) -> List[Tuple[PurePosixPath, Scope]]:
-    # Recursively construct a list mapping paths (in the code structure) to `Scope`s to `Scope`s.
+    # Recursively construct a list mapping paths (in the code structure) to
+    # `Scope`s to `Scope`s.
     scopes = list(chain(tree.iter_funcdefs(), tree.iter_classdefs()))
     # Base case.
     if len(scopes) == 0:
@@ -77,33 +94,36 @@ def paths_to_fs(
             fs_dirs.append((path, scopes[0]))
             continue
 
-        # Collapse multiple scopes into uniquely named files in their '.d' directory.
+        # Collapse multiple scopes into uniquely named files in their '.d'
+        # directory.
         for i, scope in enumerate(scopes):
             fs_dirs.append(
                 (path.with_suffix(".d") / f"{i}.{scope.type}", scope)
             )
 
-    filesystem = {}
+    filesystem: Filesystem = {}
     # Make fs directory hierarchy.
     for path, scope in fs_dirs:
         current_level = filesystem
         for i, part in enumerate(path.parts):
             # No subdirs need to be made for root dir.
-            # TODO: What to do about "weird" paths? (e.g., '/' parts in the middle).
+            # TODO: What to do about "weird" paths? (e.g., '/' parts in the
+            # middle).
             if i == 0 and part == "/":
                 continue
 
-            # Make "file" in specified parent part if this is last part it is not a directory.
+            # Make "file" in specified parent part if this is last part it is
+            # not a directory.
             if part == path.name and not part.endswith((".d", ".c")):
-                current_level[part] = scope
+                current_level[PurePosixPath(part)] = scope
                 break
 
             # Make directory if needed.
-            if part not in current_level:
-                current_level[part] = {}
+            if PurePosixPath(part) not in current_level:
+                current_level[PurePosixPath(part)] = {}
 
             # Go into subdirectory.
-            current_level = current_level[part]
+            current_level = current_level[PurePosixPath(part)]
 
     return filesystem
 
@@ -120,9 +140,9 @@ class PyCodeFS(FS):
         "supports_rename": False,
     }
 
-    def __init__(self, file_obj, language_version="3.6"):
+    def __init__(self, code_text: str, language_version: str = "3.6"):
         super().__init__()
-        self._program = parso.parse(file_obj.read(), version=language_version)
+        self._program = parso.parse(code_text, version=language_version)
         self._filesystem: Filesystem = self._build_filesystem()
 
     def _build_filesystem(self) -> Filesystem:
@@ -141,7 +161,7 @@ class PyCodeFS(FS):
             for i, part in enumerate(path_obj.parts):
                 if part == "/":
                     continue
-                current_level = current_level[part]
+                current_level = current_level[PurePosixPath(part)]
         except KeyError:
             return None
 
@@ -151,12 +171,15 @@ class PyCodeFS(FS):
         filesystem = (path_obj, resource)
         return filesystem
 
-    def getinfo(self, path, namespaces=None):
+    def getinfo(
+        self, path: str, namespaces: Optional[Collection[str]] = None
+    ) -> Info:
         # TODO: Support namespaces other than 'basic'.
         if namespaces is None:
             namespaces = ["basic"]
 
-        # XXX: Just for now, raise error when unsupported namespace encountered.
+        # XXX: Just for now, raise error when unsupported namespace
+        # encountered.
         for namespace in namespaces:
             if namespace != "basic":
                 raise NotImplementedError(
@@ -178,20 +201,27 @@ class PyCodeFS(FS):
         }
         return Info(raw_info)
 
-    def listdir(self, path):
+    def listdir(self, path: str) -> List[str]:
         info = self.getinfo(path)
         if not info.is_dir:
             raise DirectoryExpected(path)
         file_query = self._get_file_at_path(path)
         assert file_query is not None
         path_obj, resource = file_query
-        local_files = [filename for filename in resource.keys()]
+        local_files = [str(filename) for filename in resource.keys()]
         return local_files
 
-    def makedir(self, path, permissions=None, recreate=False):
+    def makedir(
+        self,
+        path: str,
+        permissions: Optional[Permissions] = None,
+        recreate: bool = False,
+    ) -> SubFS[FS]:
         raise Unsupported(msg="cannot modify pyfs")
 
-    def openbin(self, path, mode="r", buffering=-1, **options):
+    def openbin(
+        self, path: str, mode: str = "r", buffering: int = -1, **options: Any
+    ) -> BinaryIO:
         info = self.getinfo(path)
         if not info.is_file:
             raise FileExpected(path)
@@ -203,14 +233,17 @@ class PyCodeFS(FS):
         assert file_query is not None
         _, resource = file_query
         return BytesIO(
-            bytes(cast(Scope, resource).get_code(include_prefix=False), encoding="utf-8")
+            bytes(
+                cast(Scope, resource).get_code(include_prefix=False),
+                encoding="utf-8",
+            )
         )
 
-    def remove(self, path):
+    def remove(self, path: str) -> None:
         raise Unsupported(msg="cannot modify pyfs")
 
-    def removedir(self, path):
+    def removedir(self, path: str) -> None:
         raise Unsupported(msg="cannot modify pyfs")
 
-    def setinfo(self, path, info):
+    def setinfo(self, path: str, info: RawInfo) -> None:
         raise Unsupported(msg="cannot modify pyfs")
